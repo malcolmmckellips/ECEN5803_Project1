b2b9118639525e0de83130251d5e12c83f6d9dd7
Malcolm <malcolmmckellips@gmail.com>
2021-09-22 14:56:29 -0600

    added code 2

---
+++ Code 2/Code2/sqrt_approx/build/main.d
@@ -0,0 +1,3 @@
+.\build\main.o: main.c
+.\build\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\build\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
---
+++ Code 2/Code2/sqrt_approx/main.c
@@ -0,0 +1,137 @@
+/************************************************************************//**
+* \file main.c
+* \brief LAB EXERCISE 5.2 - SQUARE ROOT APPROXIMATION
+*
+* Write an assembly code subroutine to approximate the square root of an 
+* argument using the bisection method. All math is done with integers, so the 
+* resulting square root will also be an integer
+******************************************************************************
+* GOOD LUCK!
+ ****************************************************************************/
+
+ #include "stdint.h"
+ 
+ 
+ /** @cond DO_NOT_INCLUDE_WITH_DOXYGEN */
+ #include "string.h"
+ /** @endcond */
+ 
+ /**
+ * @brief 5863 Write a brief description of the function here
+ *
+ * Detailed description of the function after one line gap
+ *
+ * @note You can also add a note this way
+ *
+ * @param[in] 
+ *  You can also start writing on a new line. 
+ *  And continue on the next line. This is how you describe an input parameter
+ *
+ * @return
+ *  This is how you describe the return value
+ * 
+ */
+__asm int my_sqrt(int x){
+	
+	//Write your code here
+		//Write your code here
+//	done = 0
+//	a = 0
+//	b = square root of largest possible argument (e.g. ~216).
+//	c = -1
+//	do {
+//		c_old <- c
+//		c <- (a+b)/2
+//		if (c*c == x) {
+//			done = 1
+//		} else if (c*c < x) {
+//			a <- c
+//		} else {
+//			b <- c
+//	}
+//	} while (!done) && (c != c_old)
+//	return c 
+	
+	//r0 <- x
+	//r1 = done
+	//r2 = a
+	//r3 = b
+	//r4 = c
+	//r5 = c_old
+	//r6 = c * c 
+	
+		MOVS	r1, #0; // done = 0
+		MOVS 	r2, #0; // a = 0
+		
+		MOVS	r3, #1; 			// r3 = 1
+		LSLS r3, r3, #16 ; // r3 = r3 << 16			(b = 2^16)
+		
+		MOVS r4, #0; 			//c = 0 
+		SUBS r4, r4, #1; 	//c = -1
+		
+dowhile
+			MOVS r5, r4; //		c_old <- c
+			
+			ADDS r4, r2, r3; // c = a + b
+			ASRS r4, r4, #1; // c = (a + b) / 2
+			
+			MOVS r6, r4; 			// r6 = c
+			MULS r6, r6, r6;  // r6 = c*c 
+			CMP	 r6, r0;			// Comparing on (c^2 - x) 
+			
+			BNE notx; 		// if (c*c != X) -> notdone 
+			MOVS r1, #1; //done = 1 
+			B whilecond;	//don't need to check other if statements (if, else)
+notx
+			BGT cintob;			// if (c*c > x put c in b )
+			
+			MOVS r2, r4; 		// if (c * c < x put c into a )
+			B whilecond; 
+			
+cintob
+				MOVS r3, r4; // b <- c 
+			
+			//check while conditions 
+whilecond 
+				CMP r1, #0; // compare r1 == 0 
+				BNE donewithloop ; // if (done != 0) -> end while loop 
+			
+				CMP r4, r5; //
+				BEQ donewithloop;  //if (c == c_old) -> end while loop
+			
+				B	dowhile; 	//continue while loop if conditions are satisfied 
+				
+donewithloop
+		
+		//MOVS r3, #46341; // b = sqrt (2^31 - 1) //(or 2^16?) 
+		//MOVS r4, #-1; 
+		
+		MOVS r0, r4; //??? maybe r0 is the return value? 
+		
+		BX lr; //return
+//}
+}
+
+/*----------------------------------------------------------------------------
+ MAIN function
+ *----------------------------------------------------------------------------*/
+ /**
+ * @brief Main function
+ *
+ * Detailed description of the main
+ */
+int main(void){
+	volatile int r, j=0;
+	int i;                  
+  r = my_sqrt(0);     // should be 0
+  r = my_sqrt(25);    // should be 5
+	r = my_sqrt(133); 	// should be 11
+  for (i=0; i<10000; i++){
+		r = my_sqrt(i);
+    j+=r;
+  }
+	while(1)
+		;
+}
+
+// *******************************ARM University Program Copyright © ARM Ltd 2014*************************************/
